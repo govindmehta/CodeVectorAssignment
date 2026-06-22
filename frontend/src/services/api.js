1d4a4cb7450369c929438e553b677ac6beb0ba1e
Govind Mehta <govindmehta.gov@gmail.com>
2026-06-22 15:42:53 +0530

    small

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL =  import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api';
+const API_BASE_URL =  import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/';
 console.log(API_BASE_URL);
 
 export const fetchProducts = async ({ category, cursor }) => {
